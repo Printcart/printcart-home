25c0cce10fdc4aec9eb85488f191f7f1bc362613
Linhkinhbac <90301266+Linhkinhbac@users.noreply.github.com>
2022-04-12 10:31:14 +0700

    highlight button
    highlight button

--- src/containers/AppModern/Banner/banner.style.js
+++ src/containers/AppModern/Banner/banner.style.js
@@ -132,8 +132,9 @@ export const ButtonGroup = styled.div`
     margin-left:10px;
 
     &.primary {
-      background-color: ${themeGet('colors.white', '#fff')};
-      color: rgb(15, 33, 55);
+      background-color: ${themeGet('colors.orange', '#d1651a')};
+      background-image: -webkit-linear-gradient(180deg, #d1651a, #c1282a);
+      color: rgb(255, 255, 255);
       font-size: 16px;
       letter-spacing: -0.1px;
       &:hover {
@@ -146,6 +147,7 @@ export const ButtonGroup = styled.div`
       color: rgb(15, 33, 55);
       font-size: 16px;
       letter-spacing: -0.1px;
+      border: 3px solid #d1651a;
       &:hover {
         box-shadow: #1e2a4a 0px 12px 24px -10px;
       }
