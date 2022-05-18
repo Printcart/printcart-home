3d7c03548a8c2ee5247e727b94a02a3ac99a13fd
Linhkinhbac <90301266+Linhkinhbac@users.noreply.github.com>
2022-05-18 17:35:51 +0700

    update redirect link
    update redirect link

--- next.config.js
+++ next.config.js
@@ -9,37 +9,42 @@ const nextConfig = {
     return [
       {
         source: '/top-7-t-shirt-niche-list-and-print-on-demand-product-ideas',
-        destination: 'https://solution.printcart.com/top-7-t-shirt-niche-list-and-print-on-demand-product-ideas',
+        destination: 'https://solution.printcart.com/community/top-t-shirt-niche-list-and-print-on-demand-product-ideas',
         permanent: false,
       },
       {
         source: '/5-tips-to-succeed-in-an-online-printing-business',
-        destination: 'https://solution.printcart.com/5-tips-to-succeed-in-an-online-printing-business',
+        destination: 'https://solution.printcart.com/community/tips-to-succeed-in-an-online-printing-business',
         permanent: false,
       },
       {
         source: '/the-most-popular-print-on-demand-product-2020',
-        destination: 'https://solution.printcart.com/the-most-popular-print-on-demand-product-2020',
+        destination: 'https://solution.printcart.com/community/the-most-popular-print-on-demand-product',
         permanent: false,
       },
       {
         source: '/6-things-you-should-know-about-print-on-demand-pod',
-        destination: 'https://solution.printcart.com/6-things-you-should-know-about-print-on-demand-pod',
+        destination: 'https://solution.printcart.com/community/things-you-should-know-about-print-on-demand-pod',
         permanent: false,
       },
       {
         source: '/6-tips-to-increase-dramatically-profits-for-your-t-shirt-business',
-        destination: 'https://solution.printcart.com/6-tips-to-increase-dramatically-profits-for-your-t-shirt-business',
+        destination: 'https://solution.printcart.com/community/tips-to-increase-dramatically-profits-for-your-t-shirt-business',
         permanent: false,
       },
       {
         source: '/promoting-online-printing-designs-on-instagram',
-        destination: 'https://solution.printcart.com/promoting-online-printing-designs-on-instagram',
+        destination: 'https://solution.printcart.com/community/promoting-online-printing-designs-on-instagram',
         permanent: false,
       },
       {
         source: '/start-a-print-on-demand-t-shirt-business',
-        destination: 'https://solution.printcart.com/start-a-print-on-demand-t-shirt-business',
+        destination: 'https://solution.printcart.com/community/start-a-print-on-demand-t-shirt-business',
+        permanent: false,
+      },
+      {
+        source: '/cow-print-hot-trend-business-and-fashion-in-2020',
+        destination: 'https://solution.printcart.com/community/cow-print--hot-trend-business-and-fashion',
         permanent: false,
       },
     ]
