19be0866bc0f68c13b0d3a154efb4b8f45b2238c
DaoQuangThiet <Thietdq1711@gmail.com>
2023-07-03 09:19:06 +0700

    update

---
+++ src/containers/AppModern/BlogPage/PostDetail/TableOfContent.js
@@ -0,0 +1,238 @@
+import Box from "common/components/Box";
+import Heading from "common/components/Heading";
+import React, { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import styled from "styled-components";
+
+const WrapRight = styled(Box)`
+  position: sticky;
+  top: 25px;
+  padding: 25px;
+  border-radius: 5px;
+  box-shadow: 5px 5px 10px 4px rgb(0 0 50 / 5%);
+  margin-left: 30px;
+`;
+const TableHeading = styled(Heading)`
+  font-size: 16px;
+  color: #444;
+  text-transform: uppercase;
+`;
+
+const BoxRecap = styled(Box)`
+  .texttitle {
+    font-size: 16px;
+    font-weight: 400;
+    letter-spacing: 0.3px;
+    color: #5c5c5c;
+  }
+  .text_h2 {
+    cursor: pointer;
+    font-size: 16px;
+    color: #5c5c5c;
+    display: -webkit-box;
+    overflow: hidden;
+    margin-top: 15px;
+    font-weight: 400;
+    text-overflow: ellipsis;
+    letter-spacing: 0.3px;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    &:hover {
+      color: #2d58af;
+      font-weight: 500;
+    }
+  }
+  .textChoose_h3 {
+    cursor: pointer;
+    line-height: 1.4px;
+    font-size: 16px;
+    font-weight: 500;
+    margin-top: 10px;
+    margin-left: 20px;
+    color: #2d58af;
+    letter-spacing: 0.3px;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+  }
+  .textChoose_h2 {
+    cusor: pointer;
+    font-size: 16px;
+    font-weight: 500;
+    margin-top: 15px;
+    color: #2d58af;
+    letter-spacing: 0.3px;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    & svg {
+      font-size: 8px;
+      margin-right: 5px;
+      margin-bottom: 1px;
+    }
+  }
+  .text_h3n {
+    cursor: pointer;
+    display: none;
+    line-height: 1.4px;
+    font-size: 16px;
+    font-weight: 400;
+    margin-top: 10px;
+    margin-left: 20px;
+    color: #5c5c5c;
+    letter-spacing: 0.3px;
+    &:hover {
+      color: #2d58af;
+      font-weight: 500;
+    }
+  }
+  .text_h3b {
+    cusor: pointer;
+    line-height: 1.4px;
+    font-size: 16px;
+    font-weight: 400;
+    margin-top: 10px;
+    margin-left: 20px;
+    color: #5c5c5c;
+    letter-spacing: 0.3px;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    &:hover {
+      color: #2d58af;
+      font-weight: 500;
+    }
+  }
+`;
+const TableOfContent = ({ content }) => {
+  const [data, setData] = useState([]);
+  const [block, setBlock] = useState([]);
+  const [checkId, setCheckId] = useState();
+  const [checkOffset, setCheckOffset] = useState([]);
+
+  const { ref, inView } = useInView({
+    threshold: 0.5
+  });
+
+  const showItem = (item) => {
+    const id = item.replace(/\D/g, "");
+    const isH2 = item.startsWith("content-");
+
+    if (isH2) {
+      setBlock([]);
+      for (let i = 0; i < data?.length; i++) {
+        if (i > id) {
+          if (data[i].label.slice(0, 9) === "texttitle") {
+            setBlock((e) => [...e, data[i]]);
+          } else {
+            break;
+          }
+        }
+      }
+    }
+  };
+
+  const transferTo = (event) => {
+    const id = event.target.id;
+    const anchor = document.querySelector("#" + id);
+    showItem(id);
+    setCheckId(id);
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    }
+  };
+
+  const handleScroll = () => {
+    const headings = Array.from(
+      document.querySelectorAll(
+        ".texttitle[id^=content-], .texttitle[id^=texttitle-]"
+      )
+    );
+    const firstVisibleHeading = headings.find((heading) => {
+      const headingRect = heading.getBoundingClientRect();
+      return headingRect.top >= 0 && headingRect.top <= window.innerHeight;
+    });
+
+    if (firstVisibleHeading) {
+      showItem(firstVisibleHeading.id);
+      setCheckId(firstVisibleHeading.id);
+      setCheckOffset(firstVisibleHeading.id);
+    }
+    if (window.scrollY === 0) {
+      setBlock([]);
+      setCheckId("");
+      setCheckOffset("");
+    }
+  };
+
+  useEffect(() => {
+    if (typeof document !== "undefined" && typeof window !== "undefined") {
+      const paragraph = document.getElementsByClassName("texttitle");
+      setData([]);
+
+      for (let index = 0; index < paragraph.length; index++) {
+        paragraph[index].id =
+          paragraph[index].localName === "h2"
+            ? `content-${index}`
+            : `texttitle-${index}`;
+        setData((e) => [
+          ...e,
+          { label: paragraph[index].id, value: paragraph[index].textContent }
+        ]);
+      }
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll);
+    }
+  }, [content, checkOffset]);
+
+  return (
+    <>
+      <WrapRight>
+        <TableHeading content="On this Page" />
+        <BoxRecap>
+          {data.length > 0 ? (
+            data.map((item, index) => (
+              <p
+                key={index}
+                ref={checkOffset === item.label ? ref : null}
+                id={item.label}
+                onClick={transferTo}
+                className={
+                  (checkOffset || checkId) === item.label
+                    ? item.label.slice(0, 4) === "texttitle"
+                      ? "textChoose_h3"
+                      : "textChoose_h2"
+                    : item.label.slice(0, 4) === "texttitle"
+                    ? block.length === 0
+                      ? "text_h3n"
+                      : block.map((e) =>
+                          e.label === item.label ? "text_h3b" : "text_h3n"
+                        )
+                    : "text_h2"
+                }
+              >
+                {item.value}
+              </p>
+            ))
+          ) : (
+            <p className="texttitle">
+              {" "}
+              There is no table of contents for this article.
+            </p>
+          )}
+        </BoxRecap>
+      </WrapRight>
+    </>
+  );
+};
+export default TableOfContent;
--- src/containers/AppModern/BlogPage/PostDetail/index.js
+++ src/containers/AppModern/BlogPage/PostDetail/index.js
@@ -25,6 +25,7 @@ import GridPost, {
 } from "../blogPage.style";
 
 import Icon from "react-icons-kit";
+import TableOfContent from "./TableOfContent";
 
 const WrapBox = styled(Box)`
   margin-bottom: 50px;
@@ -100,7 +101,6 @@ const LeftDesc = styled(Box)`
 `;
 const RightDesc = styled(Box)`
   width: 30%;
-  padding-left: 30px;
   @media only screen and (max-width: 1400px) {
     width: 30%;
   }
@@ -276,18 +276,7 @@ const ContenBox = styled(Box)`
     margin-bottom: 30px;
   }
 `;
-const WrapRight = styled(Box)`
-  position: sticky;
-  top: 25px;
-  padding: 25px;
-  border-radius: 5px;
-  box-shadow: 5px 5px 10px 4px rgb(0 0 50 / 5%);
-`;
-const TableHeading = styled(Heading)`
-  font-size: 16px;
-  color: #444;
-  text-transform: uppercase;
-`;
+
 const GridBlog = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -310,98 +299,6 @@ const GridBlog = styled.div`
     }
     @media only screen and (max-width: 576px) {
       width: 100%;
-    }
-  }
-`;
-const BoxRecap = styled(Box)`
-  .texttitle {
-    font-size: 16px;
-    font-weight: 400;
-    letter-spacing: 0.3px;
-    color: #5c5c5c;
-  }
-  .text_h2 {
-    cursor: pointer;
-    font-size: 16px;
-    color: #5c5c5c;
-    display: -webkit-box;
-    overflow: hidden;
-    margin-top: 15px;
-    font-weight: 400;
-    text-overflow: ellipsis;
-    letter-spacing: 0.3px;
-    -webkit-box-orient: vertical;
-    -webkit-line-clamp: 2;
-    &:hover {
-      color: #2d58af;
-      font-weight: 500;
-    }
-  }
-  .textChoose_h3 {
-    cursor: pointer;
-    line-height: 1.4px;
-    font-size: 16px;
-    font-weight: 500;
-    margin-top: 10px;
-    margin-left: 20px;
-    color: #2d58af;
-    letter-spacing: 0.3px;
-    text-overflow: ellipsis;
-    overflow: hidden;
-    display: -webkit-box;
-    -webkit-box-orient: vertical;
-    -webkit-line-clamp: 2;
-  }
-  .textChoose_h2 {
-    cusor: pointer;
-    font-size: 16px;
-    font-weight: 500;
-    margin-top: 15px;
-    color: #2d58af;
-    letter-spacing: 0.3px;
-    text-overflow: ellipsis;
-    overflow: hidden;
-    display: -webkit-box;
-    -webkit-box-orient: vertical;
-    -webkit-line-clamp: 2;
-    & svg {
-      font-size: 8px;
-      margin-right: 5px;
-      margin-bottom: 1px;
-    }
-  }
-  .text_h3n {
-    cursor: pointer;
-    display: none;
-    line-height: 1.4px;
-    font-size: 16px;
-    font-weight: 400;
-    margin-top: 10px;
-    margin-left: 20px;
-    color: #5c5c5c;
-    letter-spacing: 0.3px;
-    &:hover {
-      color: #2d58af;
-      font-weight: 500;
-    }
-  }
-  .text_h3b {
-    cusor: pointer;
-    line-height: 1.4px;
-    font-size: 16px;
-    font-weight: 400;
-    margin-top: 10px;
-    margin-left: 20px;
-    color: #5c5c5c;
-    letter-spacing: 0.3px;
-    text-overflow: ellipsis;
-    overflow: hidden;
-    display: -webkit-box;
-    -webkit-box-orient: vertical;
-    -webkit-line-clamp: 2;
-    &:hover {
-      color: #2d58af;
-      font-weight: 500;
     }
   }
 `;
@@ -429,88 +326,6 @@ const PostDetail = (props) => {
     ?.replace(/<h3/g, '<h3 class="texttitle"')
     ?.replace(/<a/g, '<a rel="nofollow" target="_blank"')
     ?.replace(/<img/g, `<img alt="image ${topicId}"`);
-
-  const [data, setData] = useState([]);
-  const [block, setBlock] = useState([]);
-  const [checkId, setCheckId] = useState();
-  const [checkOffset, setCheckOffset] = useState();
-  const { ref, inView } = useInView({
-    threshold: 0.5
-  });
-  function showItem(item) {
-    const id = item.replace(/\D/g, "");
-    const isH2 = item.startsWith("content-");
-    if (isH2) {
-      for (let i = 0; i < data.length; i++) {
-        if (i > id) {
-          if (data[i].label.slice(0, 9) === "texttitle") {
-            setBlock((e) => [...e, data[i]]);
-          } else {
-            break;
-          }
-        }
-      }
-    }
-  }
-
-  function transferTo(event) {
-    const id = event.target.id;
-    const anchor = document.querySelector("#" + id);
-    showItem(id);
-    setCheckId(id);
-
-    if (anchor) {
-      anchor.scrollIntoView({
-        behavior: "smooth",
-        block: "start"
-      });
-    }
-  }
-
-  function handleScroll() {
-    const headings = Array.from(
-      document.querySelectorAll(
-        ".texttitle[id^=content-], .texttitle[id^=texttitle-]"
-      )
-    );
-    const firstVisibleHeading = headings.find((heading) => {
-      const headingRect = heading.getBoundingClientRect();
-      return headingRect.top >= 0 && headingRect.top <= window.innerHeight;
-    });
-
-    if (firstVisibleHeading) {
-      showItem(firstVisibleHeading.id);
-      setCheckId(firstVisibleHeading.id);
-      setCheckOffset(firstVisibleHeading.id);
-    }
-
-    if (window.scrollY === 0) {
-      setBlock([]);
-      setCheckId("");
-      setCheckOffset("");
-    }
-  }
-
-  useEffect(() => {
-    if (typeof document !== "undefined" && typeof window !== "undefined") {
-      const paragraph = document.getElementsByClassName("texttitle");
-      setData([]);
-
-      for (let index = 0; index < paragraph.length; index++) {
-        paragraph[index].id =
-          paragraph[index].localName === "h2"
-            ? `content-${index}`
-            : `texttitle-${index}`;
-        setData((e) => [
-          ...e,
-          { label: paragraph[index].id, value: paragraph[index].textContent }
-        ]);
-      }
-
-      window.addEventListener("scroll", handleScroll);
-      return () => window.removeEventListener("scroll", handleScroll);
-    }
-  }, [content, checkOffset]);
 
   return (
     <>
@@ -597,43 +412,7 @@ const PostDetail = (props) => {
                 <ContenBox>{ReactHtmlParser(content)}</ContenBox>
               </LeftDesc>
               <RightDesc>
-                <WrapRight>
-                  <TableHeading content="On this Page" />
-                  <BoxRecap>
-                    {data.length > 0 ? (
-                      data.map((item, index) => (
-                        <p
-                          key={index}
-                          ref={checkOffset === item.label ? ref : null}
-                          id={item.label}
-                          onClick={transferTo}
-                          className={
-                            (checkOffset || checkId) === item.label
-                              ? item.label.slice(0, 4) === "texttitle"
-                                ? "textChoose_h3"
-                                : "textChoose_h2"
-                              : item.label.slice(0, 4) === "texttitle"
-                              ? block.length === 0
-                                ? "text_h3n"
-                                : block.map((e) =>
-                                    e.label === item.label
-                                      ? "text_h3b"
-                                      : "text_h3n"
-                                  )
-                              : "text_h2"
-                          }
-                        >
-                          {item.value}
-                        </p>
-                      ))
-                    ) : (
-                      <p className="texttitle">
-                        {" "}
-                        There is no table of contents for this article.
-                      </p>
-                    )}
-                  </BoxRecap>
-                </WrapRight>
+                <TableOfContent content={content} />
               </RightDesc>
             </GridBlog>
             {relatedData?.data?.length > 0 && (
