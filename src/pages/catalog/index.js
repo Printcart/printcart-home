3d95c5a327d4beecfcc19abd3ed1eb7a5d112644
DaoQuangThiet <Thietdq1711@gmail.com>
2023-07-05 11:31:48 +0700

    Update index.js

--- src/pages/catalog/index.js
+++ src/pages/catalog/index.js
@@ -81,7 +81,7 @@ export async function getStaticProps() {
   const urlProduct = new URL("products", baseUrlAdmin);
   urlProduct.searchParams.set("status", "published");
   // urlProduct.searchParams.set("limit", 500);
-  urlProduct.searchParams.set("updated_at[gte]", curentDay);
+  urlProduct.searchParams.set("updated_at[lte]", curentDay);
   const paramsProduct = urlProduct.href;
 
   const fetchCollections = fetch(paramsCollection, parameter);
