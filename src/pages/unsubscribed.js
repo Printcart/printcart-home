6bb91d289648f729bbb1e072c892c58dd9c5c927
Nguyễn Duy Hiếu <126578374+hieunoobpro@users.noreply.github.com>
2024-06-12 11:51:53 +0700

    Change center display
    Change center display

--- src/pages/unsubscribed.js
+++ src/pages/unsubscribed.js
@@ -6,22 +6,31 @@ const UnsubscribeForm = () => {
   return (
     <div
       style={{
-        maxWidth: '500px',
-        margin: '200px auto',
-        padding: '1rem',
-        border: '1px solid #ccc',
-        borderRadius: '8px',
-        textAlign: 'center'
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
       }}
     >
-      <Image
-        src={mailError}
-        alt="Mail Error"
-        style={{ maxWidth: '100%', marginBottom: '1rem' }}
-      />
-      <h2 style={{ marginBottom: '0.5rem' }}>Successful unsubscribe from Printcart</h2>
-      <p style={{ fontSize: '1.2rem', marginBottom: '0' }}>You won't receive mail from our system anymore.</p>
-      <p>If this is a mistake, you can subscribe again by contacting us.</p>
+      <div
+        style={{
+          maxWidth: '500px',
+          width: '100%',
+          padding: '1rem',
+          border: '1px solid #ccc',
+          borderRadius: '8px',
+          textAlign: 'center'
+        }}
+      >
+        <Image
+          src={mailError}
+          alt="Mail Error"
+          style={{ maxWidth: '100%', marginBottom: '1rem' }}
+        />
+        <h2 style={{ marginBottom: '0.5rem' }}>Successful unsubscribe from Printcart</h2>
+        <p style={{ fontSize: '1.2rem', marginBottom: '0' }}>You won't receive mail from our system anymore.</p>
+        <p>If this is a mistake, you can subscribe again by contacting us.</p>
+      </div>
     </div>
   );
 };
