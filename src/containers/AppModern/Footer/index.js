7102e4ce844a4912b6c71077f43643b5d1ea247d
Nguyễn Duy Hiếu <126578374+hieunoobpro@users.noreply.github.com>
2024-06-17 14:50:58 +0700

    Update footer
    Update footer

--- src/containers/AppModern/Footer/index.js
+++ src/containers/AppModern/Footer/index.js
@@ -1,39 +1,58 @@
-import React from 'react';
-import Link from 'next/link';
-import Box from 'common/components/Box';
-import Text from 'common/components/Text';
-import Image from 'common/components/Image';
-import Logo from 'common/components/UIElements/Logo';
-import Heading from 'common/components/Heading';
-import Container from 'common/components/UI/Container';
+import React from "react";
+import Box from "common/components/Box";
+import Logo from "common/components/UIElements/Logo";
+import Heading from "common/components/Heading";
+import Container from "common/components/UI/Container";
 import FooterArea, {
-  WidgetArea,
   MenuArea,
-  Menu,
-  MenuItem,
   CopyrightText,
-} from './footer.style';
+  WidgetAreaContainer,
+  WidgetAreaLeft,
+  WidgetAreaRight,
+} from "./footer.style";
 
-import { footer } from 'common/data/AppModern';
+import { footer } from "common/data/AppModern";
 
 const Footer = () => {
-  const { logo, menu, widgets } = footer;
+  const { logo, menu, widgetsPartner, widgetsPOD, widgetsPrintcart } = footer;
   const date = new Date();
   const year = date.getFullYear();
 
   return (
     <FooterArea>
       <Container>
-        <WidgetArea>
-          {widgets.map((item) => (
-            <Box className="col" key={`footer-widget--key${item.id}`}>
-              <a href={item.url} target="_blank" rel="nofollow">
-                <Image src={item.icon?.src} alt={item.title} />
-              <Heading as="h3" content={item.title} />
-              <Text content={item.description} /></a>
-            </Box>
-          ))}
-        </WidgetArea>
+        <WidgetAreaContainer>
+          <WidgetAreaLeft>
+            <div className="title">For Printer Partners</div>
+            {widgetsPartner.map((item) => (
+              <Box className="col" key={`footer-widget--key${item.id}`}>
+                <a href={item.url} target="_blank" rel="nofollow">
+                  <Heading as="h3" content={item.title} />
+                </a>
+              </Box>
+            ))}
+          </WidgetAreaLeft>
+          <WidgetAreaLeft>
+            <div className="title">For POD Sellers</div>
+            {widgetsPOD.map((item) => (
+              <Box className="col" key={`footer-widget--key${item.id}`}>
+                <a href={item.url} target="_blank" rel="nofollow">
+                  <Heading as="h3" content={item.title} />
+                </a>
+              </Box>
+            ))}
+          </WidgetAreaLeft>
+          <WidgetAreaRight>
+            <div className="title">About Printcart </div>
+            {widgetsPrintcart.map((item) => (
+              <Box className="col" key={`footer-widget--key${item.id}`}>
+                <a href={item.url} target="_blank" rel="nofollow">
+                  <Heading as="h3" content={item.title} />
+                </a>
+              </Box>
+            ))}
+          </WidgetAreaRight>
+        </WidgetAreaContainer>
         {/* End of footer widgets area */}
         <MenuArea>
           <Logo
@@ -42,15 +61,6 @@ const Footer = () => {
             logoSrc={logo}
             title="App Classic"
           />
-          {/* <Menu>
-            {menu.map((item) => (
-              <MenuItem key={`footer-link${item.id}`}>
-                <Link href={item.link}>
-                  <a target="_blank" rel="nofollow">{item.text}</a>
-                </Link>
-              </MenuItem>
-            ))}
-          </Menu> */}
           <CopyrightText>Copyright {year} By PRINTCART</CopyrightText>
         </MenuArea>
         {/* End of footer menu area */}
